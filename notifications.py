4338cb524ac324c694311e22ba2075d09fbc8c0d
Ashir <122673237+MountainMan21@users.noreply.github.com>
2024-11-06 17:41:12 +0500

    Delete notifications.py

--- notifications.py
+++
@@ -1,9 +0,0 @@
-from plyer import notification
-
-def send_notification(title, message):
-    print(f"Sending notification: {title} - {message}")  # Debugging line
-    notification.notify(
-        title=title,
-        message=message,
-        timeout=10
-    )
